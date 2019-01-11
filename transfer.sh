753761dde4100fc56b1d9192d4f8633983dc0d4b
Benja <zsamora@ug.uchile.cl>
2019-01-11 14:08:45 -0300

    Version final

--- transfer.sh
+++ transfer.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-echo "Nombre del Experimento (vacio para transferir todos)"
-read EXP_NAME
+if [ -z "$1" ]; then
+  echo "Transferencia de todos los experimentos"
+else
+  EXP_NAME=$1
+  echo "Transferencia de "$EXP_NAME
+fi
 TMP=$(mktemp)
 time (nice -10 rsync -r pi@192.168.4.1:/home/pi/Camera/Data/$EXP_NAME . ) 2>$TMP
-awk -F'[ ms]+' '/^real/ {print "Tiempo de transferencia: "1000*$2"ms"}' $TMP
+awk -F'[ ms]+' '/^real/ {print "copy time: "1000*$2"ms"}' $TMP
 rm $TMP

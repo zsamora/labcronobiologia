b4e0a7b9d7e1cf32e705496af37984d939ea4281
Benja <zsamora@ug.uchile.cl>
2019-01-11 14:03:22 -0300

    Version final

--- capture.py
+++ capture.py
@@ -10,11 +10,11 @@ capt_time = None # Capture time of the actual photo
 N_FOLDERS = 0    # N of folders
 TIMELAPSE = 1    # Time interval (in seconds)
 FOLD = ""        # Actual folder
-AUX = -1         # Auxiliar variable for counting missing pictures
-SEC = -1         # Second corresponding to actual photo
-BUFFER = []      # Buffer for maximum amount of subfolders in Experiment
-INDEX_DEL = 0    # Index for deletion of older directory
-ERRORES = 0
+AUX = -1         # Previous photo second (detection of errors)
+SEC = -1         # Actual photo second
+BUFFER = []      # Buffer of subfolders in Experiment
+INDEX_DEL = 0    # Index for deletion of oldest directory
+ERRORS = 0       # Cumulative errors
 camera = picamera.PiCamera()
 
 def captureLoop():
@@ -24,15 +24,17 @@ def captureLoop():
     global FOLD
     global BUFFER
     global INDEX_DEL
-    global ERRORES
+    global ERRORS
     capt_time = datetime.now().strftime(date_format)
     FOLD = capt_time[:-4]
     SEC = int(capt_time[-2:])
-    # Actual photo is not the next photo expected
+    # Actual photo is not the next expected photo
     if (SEC - (AUX + 1)) % 60 != 0:
-        ERRORES += (SEC - (AUX + 1)) % 60
-        print("(N° errores: %s) Fotos perdidas - Dia %s, a las %s:%s del intervalo [%s,%s]" %
-        (ERRORES, capt_time[0:8],capt_time[-6:-4],capt_time[-4:-2],((AUX + 1) % 60),((SEC - 1) % 60)))
+        ERRORS += (SEC - (AUX + 1)) % 60
+        print("(N° errores: %s) Fotos perdidas - \
+                Dia %s, a las %s:%s del intervalo [%s,%s]" %
+                (ERRORS, capt_time[0:8],capt_time[-6:-4],
+                capt_time[-4:-2],((AUX + 1) % 60),((SEC - 1) % 60)))
     # The directory is not created
     if not os.path.isdir(DIR + FOLD):
         # Maximum size of folders, delete older
@@ -53,11 +55,10 @@ def captureLoop():
     # Capture photo
     try:
         camera.capture(DIR + FOLD +"/f" + capt_time + ".jpg",
-        use_video_port=True,quality=15,thumbnail=None,bayer=False)
+            use_video_port=True,quality=15,thumbnail=None,bayer=False)
         AUX = SEC
-        #print("Objects collected:", gc.get_objects())
+        # Collect garbage
         #gc.collect()
-        # print("Saved " + capt_time)
     except Exception as ex:
         print(ex)
 
@@ -69,12 +70,13 @@ def main():
     global AUX
     global BUFFER
     if (len(sys.argv[1:]) != 3):
-        print("Error de utilizacion: 'python capture1second.py max_days timelapse experiment_name'")
+        print("Error de utilizacion: 'python capture1second.py \
+                max_days timelapse experiment_name'")
     else:
-        now = datetime.now()
         TIMELAPSE = int(sys.argv[2])
         DIR = DIR + sys.argv[3] + "/"
-        N_FOLDERS = int(sys.argv[1]) * 24 + 2 # Days * 24 hr + actual folder + latest folder (in progress)
+        # Days* 24 hr + actual folder + latest folder (in progress)
+        N_FOLDERS = int(sys.argv[1]) * 24 + 2
         # If the experiment folder exists
         if os.path.isdir(DIR):
             # Get folders ordered by name
@@ -93,7 +95,8 @@ def main():
         camera.exposure_mode = 'sports'
         # Wait 2 seconds, and until miliseconds is 0
         time.sleep(2+(100-int(datetime.now().strftime('%f')[:-4]))/100.0)
-        print("Empezando la captura de fotografias: %s" % (datetime.now().strftime(date_format)))
+        print("Empezando la captura de fotografias del dia: \
+                %s" % (datetime.now().strftime(date_format)))
         # Set initial AUX
         AUX = int(datetime.now().strftime('%S'))-1
         # Call every TIMELAPSE seconds

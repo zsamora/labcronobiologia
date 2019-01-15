ea3a88150cc198e1831e3313462c77d10de92b54
Benja <zsamora@ug.uchile.cl>
2019-01-15 13:38:21 -0300

    Pruebas de mejoramiento

--- capture.py
+++ capture.py
@@ -134,7 +134,7 @@ def main():
         camera.color_effects = (128, 128)
         camera.exposure_mode = 'sports'
         camera.iso = 100
-        camera.start_preview()
+        #camera.start_preview()
         # Wait 2 seconds, and until miliseconds is 0
         time.sleep(2+(100-int(datetime.now().strftime('%f')[:-4]))/100.0)
         print("Empezando la captura de fotografias del dia: %s" %
@@ -149,63 +149,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-# Create a pool of image processors
-done = False
-lock = threading.Lock()
-pool = []
-
-class ImageProcessor(threading.Thread):
-    def __init__(self):
-        super(ImageProcessor, self).__init__()
-        self.stream = io.BytesIO()
-        self.event = threading.Event()
-        self.terminated = False
-        self.start()
-
-    def run(self):
-        # This method runs in a separate thread
-        global done
-        while not self.terminated:
-            if self.event.wait(1):
-                try:
-                    self.stream.seek(0)
-                    # Read the image and do some processing on it
-                    #Image.open(self.stream)
-                    #...
-                    #...
-                    # Set done to True if you want the script to terminate
-                    # at some point
-                    #done=True
-                finally:
-                    # Reset the stream and event
-                    self.stream.seek(0)
-                    self.stream.truncate()
-                    self.event.clear()
-                    # Return ourselves to the pool
-                    with lock:
-                        pool.append(self)
-
-def streams():
-    while not done:
-        with lock:
-            processor = pool.pop()
-        yield processor.stream
-        processor.event.set()
-
-with picamera.PiCamera() as camera:
-    pool = [ImageProcessor() for i in range (4)]
-    camera.resolution = (640, 480)
-    # Set the framerate appropriately; too fast and the image processors
-    # will stall the image pipeline and crash the script
-    camera.framerate = 10
-    camera.start_preview()
-    time.sleep(2)
-    camera.capture_sequence(streams(), use_video_port=True)
-
-# Shut down the processors in an orderly fashion
-while pool:
-    with lock:
-        processor = pool.pop()
-    processor.terminated = True
-    processor.join()
